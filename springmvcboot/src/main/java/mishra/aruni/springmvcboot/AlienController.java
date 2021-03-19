fd4aca57000a98b50dd031d155c60250b9540f89
Aruni Mishra <arunimishramsit@gmail.com>
2021-03-20 01:19:56 +0530

    content type

--- springmvcboot/src/main/java/mishra/aruni/springmvcboot/AlienController.java
+++ springmvcboot/src/main/java/mishra/aruni/springmvcboot/AlienController.java
@@ -4,9 +4,11 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +20,7 @@ public class AlienController {
 	@Autowired
 	AlienRepo alienRepo;
 
-	@GetMapping("aliens")
+	@GetMapping(path="aliens", produces = {"application/json"})
 	@ResponseBody
 	public List<Alien> getAliens() {
 		List<Alien> aliens = alienRepo.findAll();
@@ -34,9 +36,9 @@ public class AlienController {
 		return alien;
 	}
 
-	@PostMapping("alien")
+	@PostMapping(path= "alien", consumes = {MediaType.APPLICATION_JSON_VALUE})
 	@ResponseBody
-	public Alien addAlien(Alien alien) {
+	public Alien addAlien(@RequestBody Alien alien) {
 		alienRepo.save(alien);
 
 		return alien;
