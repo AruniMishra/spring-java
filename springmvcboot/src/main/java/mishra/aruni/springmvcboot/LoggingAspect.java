5d7305ce1cd84d6abbcab79becb2a12ecdbcfa56
Aruni Mishra <arunimishramsit@gmail.com>
2021-03-20 23:04:47 +0530

    AOP

--- springmvcboot/src/main/java/mishra/aruni/springmvcboot/LoggingAspect.java
+++ springmvcboot/src/main/java/mishra/aruni/springmvcboot/LoggingAspect.java
@@ -1,16 +1,38 @@
 package mishra.aruni.springmvcboot;
 
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
+
+	@Before("execution(public * mishra.aruni.springmvcboot.AlienController.getAliens())")
+	public void logBefore() {
+		LOGGER.info("getAliens invoked--Before");
+	}
 	
-	@Before("execution(public * mishra.aruni.springmvcboot.AlienController.getAliens(..))")
-	public void log() {
-		System.out.println("getAliens invoked");
+	@After("execution(public * mishra.aruni.springmvcboot.AlienController.getAliens())")
+	public void logAfterFinally() {
+		LOGGER.info("getAliens invoked--After(finally)");
+	}
+	
+	@AfterReturning("execution(public * mishra.aruni.springmvcboot.AlienController.getAliens())")
+	public void logAfterReturning() {
+		LOGGER.info("getAliens invoked successfully--AfterReturning");
+	}
+	
+	@AfterThrowing("execution(public * mishra.aruni.springmvcboot.AlienController.getAliens())")
+	public void logAfterThrowing() {
+		LOGGER.info("getAliens exception occured--AfterThrowing");
 	}
 
 }
