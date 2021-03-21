8018991fd07452a4c610900a9c50683c96f901b6
Aruni Mishra <arunimishramsit@gmail.com>
2021-03-21 23:23:46 +0530

    spring security- jpa- in progress

--- SpringSecurity/src/main/java/mishra/aruni/springsecurity/HomeController.java
+++ SpringSecurity/src/main/java/mishra/aruni/springsecurity/HomeController.java
@@ -1,11 +1,13 @@
 package mishra.aruni.springsecurity;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
 
 	@RequestMapping("/")
 	public String home() {
