16cdd1e87ae0884f85fd67d009f29a0428fe2c95
Aruni Mishra <arunimishramsit@gmail.com>
2021-03-31 21:10:09 +0530

    @Before

--- 2-spring-master/Junit/test/mishra/aruni/junit/MyMathTest.java
+++ 2-spring-master/Junit/test/mishra/aruni/junit/MyMathTest.java
@@ -2,16 +2,33 @@ package mishra.aruni.junit;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
 
 public class MyMathTest {
 
+	@BeforeClass
+	public static void beforeClass() {
+		System.out.println("@BeforeClass");
+	}
+
+	@Before
+	public void beforeEach() {
+		System.out.println("@BeforeEach");
+	}
+
 	@Test
 	public void test() {
-		
 		MyMath myMath = new MyMath();
-		
-		assertEquals(10, myMath.sum(new int[]{1,2,3,4}));
+		assertEquals(10, myMath.sum(new int[] { 1, 2, 3, 4 }));
 	}
-	
+
+	@Test
+	public void testPairSum() {
+		MyMath myMath = new MyMath();
+		assertEquals(8, myMath.sum(new int[] { 2, 6 }));
+	}
 }
