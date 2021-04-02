859f8765c6d843be542d35f8505a3070d98a8636
Aruni Mishra <arunimishramsit@gmail.com>
2021-04-02 23:08:52 +0530

    Unit testing- spring framework

---
+++ 2-spring-master/spring-boot-demo/src/test/java/mishra/aruni/springbasic/springdemo/basic/SpringDemoApplicationTests.java
@@ -0,0 +1,12 @@
+package mishra.aruni.springbasic.springdemo.basic;
+
+import org.junit.jupiter.api.Test;
+
+//@SpringBootTest
+class SpringDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
