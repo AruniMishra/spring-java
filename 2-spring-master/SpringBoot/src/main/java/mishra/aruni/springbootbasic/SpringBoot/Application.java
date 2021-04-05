d872b3ff625f72042833cef8a2c350f80806d47c
Aruni Mishra <arunimishramsit@gmail.com>
2021-04-05 10:41:48 +0530

    beans.forEach(bean -> LOGGER.info("BeanDefinitionNames: {}", bean));

--- 2-spring-master/SpringBoot/src/main/java/mishra/aruni/springbootbasic/SpringBoot/Application.java
+++ 2-spring-master/SpringBoot/src/main/java/mishra/aruni/springbootbasic/SpringBoot/Application.java
@@ -1,21 +1,26 @@
 package mishra.aruni.springbootbasic.SpringBoot;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static final Logger LOGGER= LoggerFactory.getLogger(Application.class);
-	
+
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = SpringApplication.run(Application.class, args);
-		
-		LOGGER.info("BeanDefinitionNames: {}", (Object)applicationContext.getBeanDefinitionNames());
-	}
 
+		//LOGGER.info("BeanDefinitionNames: {}", Arrays.asList(applicationContext.getBeanDefinitionNames()));
+		List<String> beans = Arrays.asList(applicationContext.getBeanDefinitionNames());
+		beans.forEach(bean -> LOGGER.info("BeanDefinitionNames: {}", bean));
+
+	}
 }
+ 
