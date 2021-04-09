f57887cfc7f77e70a743e43619d4aedf939fa2de
Aruni Mishra <arunimishramsit@gmail.com>
2021-04-09 20:57:25 +0530

    @Pointcut

---
+++ 2-spring-master/SpringBoot/src/main/java/mishra/aruni/springbootbasic/SpringBoot/aspect/CommonJoinPointConfig.java
@@ -0,0 +1,21 @@
+package mishra.aruni.springbootbasic.SpringBoot.aspect;
+
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CommonJoinPointConfig {
+	
+	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+	
+	@Pointcut("execution(* mishra.aruni.springbootbasic.SpringBoot.repo.*.*(..))")
+	public void dataLayerExecution() {
+		LOGGER.info("@Pointcut--mishra.aruni.springbootbasic.SpringBoot.repo.*.*(..) invoked");
+	}
+	
+	@Pointcut("execution(* mishra.aruni.springbootbasic.SpringBoot.service.*.*(..))")
+	public void serviceLayerExecution() {
+		LOGGER.info("@Pointcut--mishra.aruni.springbootbasic.SpringBoot.service.*.*(..) invoked");
+	}
+
+}
--- 2-spring-master/SpringBoot/src/main/java/mishra/aruni/springbootbasic/SpringBoot/aspect/UserAspect.java
+++ 2-spring-master/SpringBoot/src/main/java/mishra/aruni/springbootbasic/SpringBoot/aspect/UserAspect.java
@@ -17,6 +17,7 @@ public class UserAspect {
 
 	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
+	/*
 	@Before("execution(* mishra.aruni.springbootbasic.SpringBoot.repo.*.*(..))")
 	public void before(JoinPoint joinPoint) {
 		LOGGER.info("Before--mishra.aruni.springbootbasic.SpringBoot.repo.*.*(..) invoked - {}", joinPoint);
@@ -31,20 +32,21 @@ public class UserAspect {
 	 * joinPoint); }
 	 */
 
+	/*
 	@AfterReturning(
 			value="execution(* mishra.aruni.springbootbasic.SpringBoot.repo.*.*(..))",
 			returning="result")
 	public void afterReturning(JoinPoint joinPoint, Object result) {
 		LOGGER.info("AfterReturning-{} returned with value {}", joinPoint, result);
 	}
-	
+
 	@AfterThrowing(
 			value="execution(* mishra.aruni.springbootbasic.SpringBoot.repo.*.*(..))",
 			throwing="exception")
 	public void afterThrowing(JoinPoint joinPoint, Object exception) {
 		LOGGER.info("AfterThrowing-{} returned with value {}", joinPoint, exception);
 	}
-	
+
 	@Around(value="execution(* mishra.aruni.springbootbasic.SpringBoot.service.*.*(..))")
 	public void around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
 		long startTime= System.currentTimeMillis();
@@ -52,4 +54,22 @@ public class UserAspect {
 		long timeTaken= System.currentTimeMillis() - startTime;
 		LOGGER.info("Around- Time take by {} returned is {}", proceedingJoinPoint, timeTaken);
 	}
+
+
+	 */
+	
+	// create a common file for all pointcut
+	@Before("mishra.aruni.springbootbasic.SpringBoot.aspect.CommonJoinPointConfig.dataLayerExecution()")
+	public void beforePointcut(JoinPoint joinPoint) {
+		LOGGER.info("BeforePointcut--mishra.aruni.springbootbasic.SpringBoot.aspect.CommonJoinPointConfig.dataLayerExecution(): ", joinPoint);
+	}
+	
+	@Around("mishra.aruni.springbootbasic.SpringBoot.aspect.CommonJoinPointConfig.serviceLayerExecution()")
+	public void around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+		long startTime= System.currentTimeMillis();
+		proceedingJoinPoint.proceed();
+		long timeTaken= System.currentTimeMillis() - startTime;
+		LOGGER.info("Around- Time take by {} returned is {}", proceedingJoinPoint, timeTaken);
+	}
+	
 }
