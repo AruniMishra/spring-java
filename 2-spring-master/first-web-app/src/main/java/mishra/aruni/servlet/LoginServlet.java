5c27a36cd4d96e7623c0581a97f99a6e5e3cf445
Aruni Mishra <arunimishramsit@gmail.com>
2021-04-20 20:05:52 +0530

    httpServletRequest.setAttribute

--- 2-spring-master/first-web-app/src/main/java/mishra/aruni/servlet/LoginServlet.java
+++ 2-spring-master/first-web-app/src/main/java/mishra/aruni/servlet/LoginServlet.java
@@ -9,17 +9,21 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(urlPatterns = "/login.do")
+@WebServlet(urlPatterns = "/login")
 public class LoginServlet extends HttpServlet{
 
 	private static final long serialVersionUID = 2238069719277990176L;
-	
+
 	protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException{
-		
+
 		PrintWriter printWriter = httpServletResponse.getWriter();
-		
+
 		printWriter.println("this is a servlet");
+		String nameParameter = httpServletRequest.getParameter("name");
+		System.out.println(nameParameter);
 		
+		httpServletRequest.setAttribute("name", nameParameter);
+
 		httpServletRequest.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(httpServletRequest, httpServletResponse);
 	}
 
