21fa8b0b6eef277aa1e94e59333b25bc8d47cb16
Aruni Mishra <arunimishramsit@gmail.com>
2021-04-20 16:37:05 +0530

    LoginServlet

---
+++ 2-spring-master/first-web-app/src/main/java/mishra/aruni/servlet/LoginServlet.java
@@ -0,0 +1,24 @@
+package mishra.aruni.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/login.do")
+public class LoginServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 2238069719277990176L;
+	
+	protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException{
+		
+		PrintWriter printWriter = httpServletResponse.getWriter();
+		printWriter.println("this is a servlet");
+		
+	}
+
+}
--- 2-spring-master/first-web-app/src/main/java/webapp/LoginServlet.java
+++ 2-spring-master/first-web-app/src/main/java/webapp/LoginServlet.java
@@ -29,7 +29,7 @@ import javax.servlet.http.HttpServletResponse;
 //3. doGet(HttpServletRequest request, HttpServletResponse response)
 //4. How is the response created?
 
-@WebServlet(urlPatterns = "/login.do")
+//@WebServlet(urlPatterns = "/login.do")
 public class LoginServlet extends HttpServlet {
 
 	/**
