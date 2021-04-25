b7883ef3060f4a5e8e63afe32708c2064dcc01b3
Aruni Mishra <arunimishramsit@gmail.com>
2021-04-25 18:18:56 +0530

    spring mvc

---
+++ 2-spring-master/first-web-app-mvc/src/main/java/mishra/aruni/servlet/LoginServlet.java
@@ -0,0 +1,38 @@
+package mishra.aruni.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/login")
+public class LoginServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 2238069719277990176L;
+
+    private LoginService loginService = new LoginService();
+
+    protected void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+        String nameParameter = httpServletRequest.getParameter("name");
+        httpServletRequest.setAttribute("name", nameParameter);
+        httpServletRequest.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(httpServletRequest, httpServletResponse);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws ServletException, IOException {
+        String nameParameter = httpServletRequest.getParameter("name");
+        String passwordParameter = httpServletRequest.getParameter("password");
+        boolean userValid = loginService.isUserValid(nameParameter, passwordParameter);
+
+        if (userValid) {
+            httpServletRequest.setAttribute("name", nameParameter);
+            httpServletRequest.getRequestDispatcher("/WEB-INF/views/welcome.jsp").forward(httpServletRequest, httpServletResponse);
+        }
+        else{
+            httpServletRequest.setAttribute("error", "invalid credentials");
+            httpServletRequest.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(httpServletRequest, httpServletResponse);
+        }
+    }
+}
---
+++ 2-spring-master/first-web-app-mvc/src/main/webapp/WEB-INF/views/login.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+	pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Login here</title>
+</head>
+<body>
+	<form action="/login" method="post">
+	<p>${error}</p>
+		Name:<input type="text" name="name">
+		Password:<input type="password" name="password"> <br><br>
+		<input type="submit">
+	</form>
+</html>
