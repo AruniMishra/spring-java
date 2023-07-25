fa209bc49957ba13d8a53a68a5efe682a098c751
Aruni Mishra <arunimishramsit@gmail.com>
2023-07-25 18:04:20 +0530

    Java

---
+++ 4-examples/shedlock-demo/src/main/java/mishra/aruni/shedlockdemo/ShedlockDemoApplication.java
@@ -0,0 +1,13 @@
+package mishra.aruni.shedlockdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShedlockDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShedlockDemoApplication.class, args);
+	}
+
+}
